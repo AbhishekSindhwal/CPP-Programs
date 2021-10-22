78281a64d98ef5144282273831be2fb7f9c3cde8
Abhishek Sindhwal <87298268+AbhishekSindhwal@users.noreply.github.com>
2021-10-22 21:41:59 +0530

    Add files via upload

---
+++ PRACTICAL 8/TASK2_V1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int arr[] = {10,20,30};
+
+	cout << *arr;
+
+	cout << arr;
+
+	arr++;
+
+	cout << *arr;
+	return 0;
+}
+/*
+	This program will show error because 
+	arr is not a pointer type of variable.
+*/
---
+++ PRACTICAL 8/TASK2_V2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using  namespace std;
+int main(){
+	int arr[] = {10,20,30};
+
+	cout << *arr<<endl;
+
+	cout << arr<<endl;
+	
+
+	cout << *(arr+1)<<endl;
+	cout<<*(arr+2);
+	return 0;
+}
